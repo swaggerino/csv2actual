717e46dcdbb56b496c729ac61183d655b1624ed7
swaggerino <74626056+swaggerino@users.noreply.github.com>
2023-01-20 16:41:19 +0100

    Update dkbVisa.ts

--- src/banks/dkbVisa.ts
+++ src/banks/dkbVisa.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment';
 
 import {Bank} from './base';
 
-export class dkbvisa extends Bank {
+export class dkbVisa extends Bank {
     static description = 'DKBvisa';
 
     static requiredHeaders = [
@@ -13,14 +13,13 @@ export class dkbvisa extends Bank {
         return transactions
             .filter(transaction => transaction.belegdatum)
             .map(obj => {
-                const amount = parseFloat(obj.betrag.replace(',', '.')) || 0;
+                const amount = parseFloat(obj.betrag.replace(',', '')) || 0;
                 const payee = obj['beschreibung'];
 
                 // Read date as utc (w/out timezone) and convert to js date
                 const date = moment.utc(obj.belegdatum, 'DD.MM.YYYY').format('YYYY-MM-DD');
 
                 return {
-                    notes: obj.beschreibung,
                     date,
                     amount,
                     payee_name: payee,
